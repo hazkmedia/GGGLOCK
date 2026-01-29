4f68885c021be8c8d3d61564b5d4d36130581e5f
hazkmedia <94696371+hazkmedia@users.noreply.github.com>
2026-01-29 18:46:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Careers at Louis Vuitton</title>
+<title>Calendly - Aquent Talent</title>
 
 <style>
   html, body {
